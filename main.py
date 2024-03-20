30bc332b62c0f1786d586c7da84716f8937a50a0
sandroin <112197038+sandroin@users.noreply.github.com>
2024-03-20 22:46:29 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import sqlite3
+
+conn = sqlite3.connect('many_to_many_db.db')
+
+conn.execute("DROP TABLE IF EXISTS Relationships")
+conn.execute("DROP TABLE IF EXISTS Student")
+conn.execute("DROP TABLE IF EXISTS Advisor")
+
+conn.executescript('''
+CREATE TABLE Advisor( 
+AdvisorID INTEGER NOT NULL PRIMARY KEY, 
+AdvisorName TEXT NOT NULL
+);
+
+CREATE TABLE Student( 
+StudentID INTEGER NOT NULL PRIMARY KEY, 
+StudentName TEXT NOT NULL, 
+AdvisorID INTEGER, 
+FOREIGN KEY(AdvisorID) REFERENCES Advisor(AdvisorID)
+); 
+
+CREATE TABLE Relationships(
+StudentID INTEGER NOT NULL, 
+AdvisorID INTEGER NOT NULL, 
+FOREIGN KEY(StudentID) REFERENCES Student(StudentID), 
+FOREIGN KEY(AdvisorID) REFERENCES Advisor(AdvisorID), 
+PRIMARY KEY(StudentID, AdvisorID)
+);
+
+INSERT INTO Advisor(AdvisorID, AdvisorName) VALUES 
+(1,"John Paul"), 
+(2,"Anthony Roy"), 
+(3,"Raj Shetty"), 
+(4,"Sam Reeds"), 
+(5,"Arthur Clintwood"); 
+
+INSERT INTO Student(StudentID, StudentName, AdvisorID) VALUES 
+(501,"Geek1", 1), 
+(502,"Geek2", 1), 
+(503,"Geek3", 2), 
+(504,"Geek4", 3), 
+(505,"Geek5", 4), 
+(506,"Geek6", 1), 
+(507,"Geek7", 2), 
+(508,"Geek8", 3), 
+(509,"Geek9", NULL), 
+(510,"Geek10", 4); 
+
+INSERT INTO Relationships(StudentID, AdvisorID) VALUES
+(501, 1),
+(502, 1),
+(503, 2),
+(501, 2),
+(501, 3),
+(502, 4),
+(503, 4),
+(506, 5),
+(507, 5),
+(510, 1),
+(503, 3),
+(501, 5),
+(507, 4),
+(508, 4),
+(509, 3),
+(510, 5),
+(510, 2),
+(506, 2), 
+(504, 4);
+''')
+
+conn.execute("DROP TABLE IF EXISTS Advisor_Student_Relationship")
+conn.execute('''CREATE TABLE Advisor_Student_Relationship AS SELECT a.AdvisorID, a.AdvisorName, s.StudentID,
+s.StudentName FROM Advisor AS a INNER JOIN Relationships AS r ON a.AdvisorID = r.AdvisorID
+INNER JOIN Student AS s ON s.StudentID = r.StudentID
+''')
+
+conn.commit()
+
+result = conn.execute('''SELECT a.AdvisorID, a.AdvisorName, COUNT(a.StudentID) FROM Advisor_Student_Relationship AS a
+GROUP BY a.AdvisorID;
+''')
+for i in result.fetchall():
+    print("Advisor {} with ID {} has {} students.".format(i[1], i[0], i[2]))
+
+conn.close()
